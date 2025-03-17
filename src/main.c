f57aebb340457ce333ba549f588e190986b11aa8
Jayleeva <101644976+Jayleeva@users.noreply.github.com>
2025-03-17 13:53:02 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,34 @@
 #include "../inc/minishell.h"
 #include "../libft/inc/libft.h"
 
+/*void	handle_signal(int sig, siginfo_t *info, void *ucontext)
+{
+	(void)ucontext;
+	(void)info;
+
+}*/
+
 int	main(void)
 {
-	static char *line_read = (char *)NULL;
-	line_read = readline("Enter a line: ");
-	add_history(line_read);
-	printf("%s\n", ttyname(1));
-	
+	char 		*line_read = (char *)NULL;
+	/*sigset_t			set;
+	struct sigaction	shell;
+
+	sigemptyset(&set);
+	sigaddset(&set, SIGUSR1);
+	sigaddset(&set, SIGUSR2);
+	shell.sa_flags = SA_SIGINFO | SA_RESTART;
+	shell.sa_mask = set;
+	shell.sa_sigaction = &handle_signal;*/
+
+	while (1)
+	{
+		line_read = readline("Enter a line: ");
+		//add_history(line_read);
+		if (ft_strncmp(line_read, "cd", 3) == 0)
+			ft_printf("cd");
+		if (ft_strncmp(line_read, "exit", 5) == 0)
+			break;
+	}
+	//printf("%s\n", ttyname(1));
 }
-
-
-
-
