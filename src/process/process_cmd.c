0d483c097a6d6c90cf990a05c66146f560ab20b8
orifcg <cynthia.glardon@formation.orif.ch>
2025-04-16 10:32:53 +0000

    added mishandling of exit and env

--- src/process/process_cmd.c
+++ src/process/process_cmd.c
@@ -34,6 +34,21 @@ void    process_other(char *cmd, t_data *data)
     free(s);
 }
 
+
+int ft_isnum(char *s)
+{
+    int i;
+
+    i = 0;
+    while (s[i])
+    {
+        if (!ft_isdigit(s[i]))
+            return (0);
+        i ++;
+    }
+    return (1);
+}
+
 void    process_token_list(t_data *data, t_token *token_list)
 {
     t_token *current;
@@ -43,11 +58,40 @@ void    process_token_list(t_data *data, t_token *token_list)
         return ;
     
     if (!ft_strncmp(current->str, "exit", 5)) // si autre argument après, message d'erreur puis crash? vérifier dans bash
+    {
+        if (current->next && current->next->type == ARG)
+        {
+            if (ft_isnum(current->next->str))
+            {
+                if (current->next->next)
+                {
+                    ft_printf("exit\nminishell: exit: too many arguments\n");
+                    return ;
+                }
+                ft_printf("exit\n");
+                exit (ft_atoi(current->next->str) % 256);
+            }
+            else
+            {
+                ft_printf("exit\nminishell: exit: '%s': numeric argument required\n", current->next->str);
+                exit (2);
+            }
+        }
         process_exit();
+    }
     else if (!ft_strncmp(current->str, "pwd", 4)) //si autre argument après, l'ignore et agit normalement? vérifier dans bash
         process_pwd(data);
     else if (!ft_strncmp(current->str, "env", 4)) // si autre argument après est quelque chose qui n'existe pas, message d'erreur n'existe pas, si existe, rien ne se passe car pas d'environnement là-bas? vérifier dans bash
+    {
+        if (current->next)
+        {
+            //if ()
+            //  return;
+            ft_printf("env: '%s': No such file or directory\n", current->next->str);
+            return ;
+        }    
         process_env(data);
+    }
     else if (!ft_strncmp(current->str, "cd", 2))
     {
         if (!current->next) // si pas d'argument donné, retour à HOME.
